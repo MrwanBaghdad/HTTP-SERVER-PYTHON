acb52ebcfe8aac476c59ab6cc683cf103ef811fb
MrwanBaghdad <mrwanbaghdad76@gmail.com>
2016-11-05 15:03:52 +0200

    serving headers and data as http 1.0

--- test.py
+++ test.py
@@ -19,11 +19,31 @@
 
 # print(sys.argv);
 
-import threading
-import os
+import socket;
+import time;
+import os;
+with open("res/index.html",mode='r',buffering=1024) as f:
+    print(f.read())
 
-value=3
+#Serving files buffereing with http 1.0:
 
+def serving_http_1.0():
+    flag=False
+    try:
+        with open(parserd['url'],mode='r',buffering=conn_buffer_size) as File:
+            if not flag :
+                conn.sendall(serv_http_1.0_header("OK"))
+                flag=True;
+            conn.sendall(File.open().encode()) # if raw send without encoding 
+        conn.shutdown(socket.SHUT_RD);
+    except FileNotFoundError as X:
+        serv_http_1.0_header("NOT FOUND");
+    time.sleep(0.5);
+    conn.close();
 
-x=threading.Thread()
-    
+def serv_http_1.0_header(arg):
+    if arg.upper() =="OK":
+        headr_str= "HTTP /1.0 200 OK"
+        return headr_str.encode();
+    elif arg.upper() =="NOT FOUND":
+        headr_str = "HTTP /1.0 404 NOT FOUND"
