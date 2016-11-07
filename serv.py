5b9ecda3189d1358673187d0276ec97d54a648d8
MrwanBaghdad <mrwanbaghdad76@gmail.com>
2016-11-07 03:55:40 +0200

    styling

--- serv.py
+++ serv.py
@@ -1,64 +1,65 @@
-import socket;
-import re;
-import os;
-import parse;
-import time;
+import socket
+import re
+import os
+import parse
+import time
+
+HOST = ''
+PORT = 5007
 
 
-host=''
-port= 5007
 
+SERVER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER_SOCKET.bind((HOST, PORT))
 
-
-server_socekt= socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-server_socekt.bind( (host,port) );
-
-while(1):
-    server_socekt.listen(1);
-    conn,addr=server_socekt.accept();
-    with conn:
-        print('connected by ',addr);
+while 1:
+    SERVER_SOCKET.listen(1)
+    CONN, ADDR = SERVER_SOCKET.accept()
+    with CONN:
+        print('connected by ', ADDR)
         while True:
-            data=conn.recv(1024)
-            conn.shutdown(socket.SHUT_RD);
+            data = CONN.recv(1024)
+            CONN.shutdown(socket.SHUT_RD)
             # print(parse.req(data.decode()));
-            # conn.sendall(b'
-            
-            parsed=parse.req(data.decode())
-            print(parsed);
-            #TODO: catch FileNotFoundError;
-            with open("res"+parsed["url"],mode='r',buffering=1024) as f:
-                conn.sendall(f.read().encode());
+            # CONN.sendall(b'
 
-            conn.shutdown(socket.SHUT_RDWR);
+            parsed = parse.req(data.decode())
+            print(parsed)
+
+    # TODO : catch FileNotFoundError
+            with open("res"+parsed["url"], mode='r', buffering=1024) as f:
+                CONN.sendall(f.read().encode())
+
+            CONN.shutdown(socket.SHUT_RDWR)
             # time.sleep(1);
-            conn.close();
-            print(conn);
-            break;
-            # server_socekt.close();
+            CONN.close()
+            print(CONN)
+            break
+            # SERVER_SOCKET.close();
 
-def serve_get(conn,url):
+def serve_get(conn, url):
     with conn:
         try:
-            with open(url,mode='r',buffering=1024) as f:
-                x=f.read()
+            with open(url, mode='r', buffering=1024) as f:
+                x = f.read()
                 conn.send(x.encode())
         except FileNotFoundError as err:
             conn.sendall(b'404 NOT FOUND')
+            print(err)
             return
 
-# TODO: change 1024 to buffer_size
-def serve_post(conn,url):
+# FIXME: change 1024 to buffer_size 
+def serve_post(conn, url):
     with conn:
-        with open(url,mode='w+',buffering=1024) as f:
-            data=conn.recv(1024);
-            data=data.decode();
-            f.write(data);
+        with open(url, mode='w+', buffering=1024) as f:
+            data = conn.recv(1024)
+            data = data.decode()
+            f.write(data)
 
-def serve_master(conn,data):
-    p= parse.req(data.decode());
+def serve_master(conn, data):
+    p = parse.req(data.decode());
     if p['method'] == "GET":
-        serve_get(p['url']);
-    else if p['method']=="POST":
+        serve_get(conn, p['url']);
+    elif p['method'] == "POST":
         conn.sendall(b"200 OK");
-        serve_post(conn,p['url'])
+        serve_post(conn, p['url'])
